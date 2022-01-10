ee42cb46ad87cb90d085aed7713e415647d65295
Tom-Erik Thorbeck <Tomerik@thorbeck.no>
2022-01-10 19:44:23 +0100

    Build

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,13 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
 	}
 };
 
