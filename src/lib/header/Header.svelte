189e9cd42f0aa45b5dcbc4b0e322ef800a9ff35f
Tom-Erik Thorbeck <tomerik@thorbeck.no>
2022-01-10 12:47:08 +0100

    Removed demo code. Init css.

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,124 +1,115 @@
 <script>
 	import { page } from '$app/stores';
-	import logo from './svelte-logo.svg';
+	import logo from './logo.svg';
 </script>
 
 <header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
-	</div>
-
-	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
+	<h1>
+		<svg
+			version="1.1"
+			id="Layer_1"
+			xmlns="http://www.w3.org/2000/svg"
+			xmlns:xlink="http://www.w3.org/1999/xlink"
+			x="0px"
+			y="0px"
+			viewBox="0 0 1080 1080"
+			style="enable-background:new 0 0 1080 1080;"
+			xml:space="preserve"
+		>
+			<style type="text/css">
+				.st0 {
+					fill: #f29e04;
+				}
+				.st1 {
+					fill: #ffffff;
+				}
+			</style>
+			<g>
+				<path
+					class="st0"
+					d="M93,774c-5.5,0-10-4.5-10-10V649c0-5.5,4.5-10,10-10c54,0,98-44,98-98s-44-98-98-98c-5.5,0-10-4.5-10-10V317
+		c0-5.5,4.5-10,10-10h894c5.5,0,10,4.5,10,10v115c0,5.5-4.5,10-10,10c-54,0-98,44-98,98s44,98,98,98c5.5,0,10,4.5,10,10v116
+		c0,5.5-4.5,10-10,10H93z"
+				/>
+				<path
+					class="st1"
+					d="M987,317v115c-59.6,0-108,48.4-108,108c0,59.6,48.4,108,108,108v116H540H93V649c59.6,0,108-48.4,108-108
+		c0-59.6-48.4-108-108-108V317h447H987 M987,297H540H93c-11,0-20,9-20,20v116c0,11,9,20,20,20c48.5,0,88,39.5,88,88s-39.5,88-88,88
+		c-11,0-20,9-20,20v115c0,11,9,20,20,20h447h447c11,0,20-9,20-20V648c0-11-9-20-20-20c-48.5,0-88-39.5-88-88s39.5-88,88-88
+		c11,0,20-9,20-20V317C1007,306,998,297,987,297L987,297z"
+				/>
+			</g>
+			<path
+				class="st1"
+				d="M723,337c-5.5,0-10-4.5-10-10v-10c0-5.5,4.5-10,10-10s10,4.5,10,10v10C733,332.5,728.5,337,723,337z"
+			/>
+			<path
+				class="st1"
+				d="M723,708.3c-5.5,0-10-4.5-10-10v-18.6c0-5.5,4.5-10,10-10s10,4.5,10,10v18.6C733,703.8,728.5,708.3,723,708.3z"
+			/>
+			<path
+				class="st1"
+				d="M723,634c-5.5,0-10-4.5-10-10v-18.6c0-5.5,4.5-10,10-10s10,4.5,10,10V624C733,629.6,728.5,634,723,634z"
+			/>
+			<path
+				class="st1"
+				d="M723,559.8c-5.5,0-10-4.5-10-10v-18.6c0-5.5,4.5-10,10-10s10,4.5,10,10v18.6C733,555.3,728.5,559.8,723,559.8z"
+			/>
+			<path
+				class="st1"
+				d="M723,485.5c-5.5,0-10-4.5-10-10V457c0-5.5,4.5-10,10-10s10,4.5,10,10v18.6C733,481,728.5,485.5,723,485.5z"
+			/>
+			<path
+				class="st1"
+				d="M723,411.3c-5.5,0-10-4.5-10-10v-18.6c0-5.5,4.5-10,10-10s10,4.5,10,10v18.6C733,406.8,728.5,411.3,723,411.3z"
+			/>
+			<path
+				class="st1"
+				d="M723,774c-5.5,0-10-4.5-10-10v-10c0-5.5,4.5-10,10-10s10,4.5,10,10v10C733,769.5,728.5,774,723,774z"
+			/>
 		</svg>
-		<ul>
-			<li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li>
-			<li class:active={$page.url.pathname === '/about'}>
-				<a sveltekit:prefetch href="/about">About</a>
-			</li>
-			<li class:active={$page.url.pathname === '/todos'}>
-				<a sveltekit:prefetch href="/todos">Todos</a>
-			</li>
-		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
-	</nav>
-
-	<div class="corner">
-		<!-- TODO put something else here? github link? -->
-	</div>
+		<span>The Rafflinator</span>
+	</h1>
 </header>
 
 <style>
 	header {
 		display: flex;
-		justify-content: space-between;
+		justify-content: center;
+		padding: 1rem;
+		background: var(--color-blue-mike);
+	}
+	header:hover svg {
+		animation-iteration-count: infinite;
 	}
 
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
+	h1 {
 		display: flex;
+		margin: 0;
+		gap: 1rem;
+		user-select: none;
+		justify-content: center;
 		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
-
-	nav {
-		display: flex;
-		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
 	}
 
 	svg {
-		width: 2em;
-		height: 3em;
+		width: 1em;
+		height: 1em;
 		display: block;
+		transform-origin: center center;
+		transform: rotateZ(-45deg);
+		animation: shakeRaffle 140ms ease-in-out 0;
 	}
 
-	path {
-		fill: var(--background);
-	}
-
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background: var(--background);
-		background-size: contain;
-	}
-
-	li {
-		position: relative;
-		height: 100%;
-	}
-
-	li.active::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--accent-color);
-	}
-
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 1em;
-		color: var(--heading-color);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
-		text-decoration: none;
-		transition: color 0.2s linear;
-	}
-
-	a:hover {
-		color: var(--accent-color);
+	@keyframes shakeRaffle {
+		0% {
+			transform: rotateZ(-60deg);
+		}
+		50% {
+			transform: rotateZ(-30deg);
+		}
+		100% {
+			transform: rotateZ(-45deg);
+		}
 	}
 </style>
